25a5fd1c80653627f2fecf3652b14ba4dbdb2522
William Ma <mawilliam.v20@gmail.com>
2024-11-29 16:39:14 -0500

    added some recurse stuff

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,10 @@
 """Evaluation tools"""
+
 import itertools
 
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 
 def draw_pie(xpos, ypos, dist, colors=None, ax=None, **kwargs):
@@ -61,6 +63,14 @@ def recurse_mean_dict(d, d_mean):
             d_mean[k].append(np.mean(v))
 
 
+def recurse_dict_to_numpy(d):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            recurse_dict_to_numpy(d[k])
+        elif torch.is_tensor(v):
+            d[k] = v.detach().cpu().numpy()
+
+
 def recurse_avg_dict(d, d_avg, div_by_weights=False):
     """Recursively average the values of dict `d` of lists and append into dict
     `d_avg` of lists.
@@ -78,9 +88,7 @@ def recurse_avg_dict(d, d_avg, div_by_weights=False):
             recurse_avg_dict(v, d_avg[k], div_by_weights)
         else:
             if div_by_weights and k not in {"lr", "weights"}:
-                d_avg[k].append(
-                    np.average(np.divide(v, d["weights"]), weights=d["weights"])
-                )
+                d_avg[k].append(np.average(np.divide(v, d["weights"]), weights=d["weights"]))
             else:
                 d_avg[k].append(np.average(v, weights=d["weights"]))
 
@@ -133,9 +141,7 @@ def format_iters(nested_list, startpoint=False, endpoint=True):
     else:
         for i, l in enumerate(nested_list):
             if not endpoint and i == len(nested_list) - 1:
-                x_i = np.linspace(
-                    i, i - 1, len(l + 1), endpoint=False, dtype=np.float32
-                )
+                x_i = np.linspace(i, i - 1, len(l + 1), endpoint=False, dtype=np.float32)
                 x_i = x_i[1:]
             else:
                 x_i = np.linspace(i, i - 1, len(l), endpoint=False, dtype=np.float32)
